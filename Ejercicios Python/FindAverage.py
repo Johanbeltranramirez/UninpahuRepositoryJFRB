f91daf91996fd8f0c704b4d54b3ff1d9b2088bcb
Johanbeltranramirez <johanframirez13@gmail.com>
2025-02-17 23:31:53 -0500

    Add dir for python exercises

---
+++ Ejercicios Python/FindAverage.py
@@ -0,0 +1,23 @@
+# Lista de nombres
+names = ["Juan", "Felipe", "Angie"]
+
+
+for i in range(len(names)):
+    print(f"names[{i}]: {names[i]}")
+
+
+#Lista de valores
+values = [10, 20, 30, 40, 50]
+
+#Calcula la media
+mean = sum(values) / len(values)
+
+#Calcula la varianza
+variance = sum((x - mean) ** 2 for x in values) / len(values)
+
+#resultados
+print(f"Valores: {values}")
+print(f"Media (μ): {mean}")
+print(f"Varianza (σ²): {variance}")
+
+print(f"Promedio de la varianza: {variance}")
