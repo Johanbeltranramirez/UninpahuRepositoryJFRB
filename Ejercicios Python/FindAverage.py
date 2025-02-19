b76ef3ccd02ef2595c6c18caa55d19486d51828c
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2025-02-18 20:57:51 -0500

    Update FindAverage.py

--- Ejercicios Python/FindAverage.py
+++ Ejercicios Python/FindAverage.py
@@ -9,15 +9,13 @@ for i in range(len(names)):
 #Lista de valores
 values = [10, 20, 30, 40, 50]
 
-#Calcula la media
-mean = sum(values) / len(values)
+media = sum(values) / len(values)
 
-#Calcula la varianza
-variance = sum((x - mean) ** 2 for x in values) / len(values)
+variance = sum((x - media) ** 2 for x in values) / len(values)
 
 #resultados
 print(f"Valores: {values}")
-print(f"Media {mean}")
+print(f"Media {media}")
 print(f"Varianza: {variance}")
 
 print(f"Promedio de la varianza: {variance}")
