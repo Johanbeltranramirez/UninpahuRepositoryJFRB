abfc883e3f3fb6ec331a297693fd95e57e5415d6
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2025-02-25 21:21:36 -0500

    Update FindAverage.js

--- Ejercicios Python/FindAverage.js
+++ Ejercicios Python/FindAverage.js
@@ -35,6 +35,6 @@ function varianceFnc(numbers, mean) {
 //Sample data: heights in millimeters
 let heights = [600, 470, 170, 430, 300];
 
-//Compute variance and standard deviation
+//Compute variance and standard deviation, calling function varianceFnc
 const variance = varianceFnc(heights, average(heights));
 console.log("Standard Deviation:", Math.sqrt(variance)); //The compute of the standard deviation is the square root of the variance v
