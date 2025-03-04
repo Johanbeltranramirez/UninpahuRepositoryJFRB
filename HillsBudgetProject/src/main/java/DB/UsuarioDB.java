9d6b0c3f941099ef3a00374a040a1339ebddfd69
Johanbeltranramirez <114593412+Johanbeltranramirez@users.noreply.github.com>
2025-03-03 21:30:28 -0500

    Update UsuarioDB.java

--- HillsBudgetProject/src/main/java/DB/UsuarioDB.java
+++ HillsBudgetProject/src/main/java/DB/UsuarioDB.java
@@ -1,29 +1,44 @@
 package DB;
-
 import Models.Usuario;
+import Models.PresupuestoGeneral;
+import Models.Aprobacion;
+import java.util.ArrayList;
 
 
-public class UsuarioDB{
+public class Database{
     
-    public Usuario[] dbUsuario;
+    ArrayList<Usuario> lstUsuarios;
+    ArrayList<PresupuestoGeneral> lstPresupuestos;
+    ArrayList<Aprobacion> lstAprobaciones;
     
-    public UsuarioDB(){
-        dbUsuario = new Usuario[10];
-        this.getUsuario();
+
+    
+    public Database(){
     }
     
     public void getUsuario(){
-        Usuario usuario = new Usuario(1, "Jhoan Ramirez", "jhoan123@gmail.com", "administrador");
-        Usuario usuario2= new Usuario(2, "Andres Jimenez", "Jimenez02@gmail.com", "finanzas");
-        Usuario usuario3 = new Usuario(3, "Maria Gonzalez", "maria.g@gmail.com", "administrador");
-        Usuario usuario4 = new Usuario(4, "Carlos Perez", "carlos.perez@yahoo.com", "contable");
-        Usuario usuario5 = new Usuario(5, "Laura Torres", "laura.t@gmail.com", "finanzas");
+        lstUsuarios = new ArrayList<>();
         
-        dbUsuario[0] = usuario;
-        dbUsuario[1] = usuario2;
-        dbUsuario[2] = usuario3;
-        dbUsuario[3] = usuario4;
-        dbUsuario[4] = usuario5;
-}
+        lstUsuarios.add(new Usuario(1, "Johan Ramirez", "jhoan123@gmail.com", "administrador"));
+        lstUsuarios.add(new Usuario(2, "Andres Jimenez", "Jimenez02@gmail.com", "finanzas"));
+        lstUsuarios.add(new Usuario(3, "Maria Gonzalez", "maria.g@gmail.com", "administrador"));
+        lstUsuarios.add(new Usuario(4, "Carlos Perez", "carlos.perez@yahoo.com", "contable"));
+        lstUsuarios.add(new Usuario(5, "Laura Torres", "laura.t@gmail.com", "finanzas"));
+
+    }
+    
+        public void getPresupuestoGeneral(){
+        lstPresupuestos = new ArrayList<>();
+        
+        lstPresupuestos.add(new PresupuestoGeneral(1, "Presupuesto 2025", "Presupuesto anual para operaciones generales", 50000, "Pendiente", '02/03/2025'));
+
+    }
+        
+    public void getAprobaciones(){
+        lstAprobaciones = new ArrayList<>();
+        
+        lstAprobaciones.add(new Aprobacion(1, "Presupuesto 2025", 1, "pendiente" ));
+        
+    }
     
 }
