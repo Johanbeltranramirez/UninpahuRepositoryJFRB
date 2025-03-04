2d3c8ec0d3d471500d2f2c3771647d9514feccb2
judynatalialopez <137812850+judynatalialopez@users.noreply.github.com>
2025-03-03 20:15:24 -0500

    Update PresupuestoGeneralDB.java

--- HillsBudgetProject/src/main/java/DB/PresupuestoGeneralDB.java
+++ HillsBudgetProject/src/main/java/DB/PresupuestoGeneralDB.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DB;
 
-/**
- *
- * @author johan
- */
-public class PresupuestoGeneralDB {
+import Models.PresupuestoGeneral;
+
+
+public class PresupuestoGeneralDB{
+    
+    public PresupuestoGeneral[] dbPresupuestoGeneral;
+    
+    public PresupuestoGeneralDB(){
+        dbPresupuestoGeneral = new PresupuestoGeneral[10];
+        this.getPresupuestoGeneral();
+    }
+    
+    public void getPresupuestoGeneral(){
+        PresupuestoGeneral presupuestoGeneral = new PresupuestoGeneral(1, "Presupuesto 2025", "Presupuesto anual para operaciones generales", 50000, "Pendiente", '02/03/2025');
+        
+        PresupuestoGeneral[0] = presupuestoGeneral;
+        
     
 }
